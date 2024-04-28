debc89c5a23ad0cfbdc5dd67a8ca58e16567bae6
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-04-28 14:53:27 +0300

    Update Footer component styling in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <section className="bg-dark-hard">
-      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10"></footer>
+      <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10 "></footer>
     </section>
   );
 };
