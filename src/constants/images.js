45276817f128f180cf3935aaf0afc01162454dc5
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-04-28 14:52:47 +0300

    Add new images to images.js

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,11 @@
 import LogoImage from "@/assets/Logo.jpg";
+import HeroImage from "@/assets/HeroImage.jpg";
+import CharacterHead from "@/assets/character-head.jpg";
 
 const images = {
   LogoImage,
+  HeroImage,
+  CharacterHead,
 };
 
 export default images;
