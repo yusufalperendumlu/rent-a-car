04817444ccad224aa9149f794a435630a14eeb31
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-05-04 23:14:50 +0300

    feat: Add profile and car show pages
    The code changes include adding new routes and components for the profile and car show pages.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "@/pages/home/HomePage";
 import RegisterPage from "@/pages/register/RegisterPage";
+import ProfilePage from "@/pages/profile/ProfilePage";
+import CarsShow from "@/pages/carsShow/CarsShow";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/rent/1" element={<CarsShow />} />
         </Routes>
       </div>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>
 );
