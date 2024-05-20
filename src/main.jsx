35a96f1ae5bb7a586c50ddd7d607f309fb540486
yusufalperendumlu <yusufalperendumlu@gmail.com>
2024-05-20 03:30:09 +0300

    refactor: Add Provider component to main.jsx for Redux store
    Update the main.jsx file to import the Provider component from "react-redux" and wrap the existing QueryClientProvider component with it. This change allows the Redux store to be accessed by the components in the application. By providing the store prop to the Provider component and wrapping it around the QueryClientProvider component, the application can now access the Redux store and utilize its state management capabilities.

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
 
+import store from "./store";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
   </BrowserRouter>
 );
